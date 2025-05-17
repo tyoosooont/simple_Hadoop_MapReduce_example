5db18e4101071e1e223a3ed3dd55780b4cc0b228
Tyoosoon <tyoosoon@example.com>
2025-05-17 04:00:47 +0000

    Updated mapper.py with extended stopwords and reprocessed result3

--- mapper.py
+++ mapper.py
@@ -2,20 +2,31 @@
 import sys
 import re
 
-# Define a basic list of English stopwords
-stopwords = {
+# Modern English stopwords
+modern_stopwords = {
     'the', 'and', 'to', 'of', 'a', 'in', 'that', 'it', 'is', 'was', 'he', 'for',
     'on', 'with', 'as', 'his', 'i', 'at', 'by', 'an', 'be', 'this', 'have', 'from',
     'or', 'one', 'had', 'not', 'but', 'what', 'all', 'were', 'we', 'when', 'your',
     'can', 'there', 'their', 'so', 'if', 'me', 'my', 'them', 'no', 'are', 'you', 'do',
-    'who', 'which', 'will', 'would', 'could', 'should', 'been', 'he', 'she', 'they',
-    'them', 'i', 'you', 'its', 'then', 'because', 'into', 'out', 'about'
+    'him', 'her', 'will', 'our', 'now', 'they', 'she', 'would', 'which', 'how', 'am'
 }
 
+# Shakespearean stopwords
+shakespearean_stopwords = {
+    'thou', 'thy', 'thee', 'thine', 'ye', 'hath', 'doth',
+    'art', 'wilt', 'shalt', 'canst', 'couldst', 'wouldst',
+    'shouldst', 'mayst', 'mightst', 'must', 'didst', 'wert',
+    'tis', 'twas', 'oft', 'nay', 'aye', 'ere', 'wherefore',
+    'shall', 'o'
+}
+
+# Combined stopwords
+stopwords = modern_stopwords.union(shakespearean_stopwords)
+
+# Process each line from standard input
 for line in sys.stdin:
     line = line.strip().lower()
-    # Remove punctuation
-    line = re.sub(r'[^\w\s]', '', line)
+    line = re.sub(r'[^\w\s]', '', line)  # remove punctuation
     words = line.split()
     for word in words:
         if word and word not in stopwords:
